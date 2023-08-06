887bafc7f691a1928bf9ea086e659f693d6d71c0
Gabrielshow <adefelashogbanmu@gmail.com>
2023-08-06 10:16:58 +0100

    first three problems

---
+++ h003.hs
@@ -0,0 +1,11 @@
+main = putStrLn (show ans)
+ans = largestPrimeFactor (600851475143 :: Integer)
+
+largestPrimeFactor n =
+	let
+		p = smallestPrimeFactor n
+	in
+		if p == n then p
+		else largestPrimeFactor (div n p)
+
+smallestPrimeFactor n = head [k | k <- [2..n], mod n k == 0]
